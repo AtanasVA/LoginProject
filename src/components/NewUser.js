1c53bbd2b7dde2007074711d7e0219bd1414ae6d
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2022-08-23 09:56:06 +0300

    before splitting NewUser and NewUserData

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NewUser.module.css";
+import Container from "./UI/Container";
+import RenderUser from "./RenderUser";
 
 const NewUser = () => {
-  return (
-    <div className={styles.containter}>
-      <label className={styles.username} for="username">
-        Username
-      </label>
-      <input
-        className={styles["username-field"]}
-        type="text"
-        id="username"
-      ></input>
-      <label className={styles.age} for="age">
-        Age (Years)
-      </label>
-      <input className={styles["age-field"]} type="number" id="age"></input>
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
 
-      <input className={styles.submit} type="button" value="Add user"></input>
-    </div>
+  const logUsername = (event) => {
+    setUsername(event.target.value);
+  };
+  const logAge = (event) => {
+    setAge(event.target.value);
+  };
+
+  const submitData = (event) => {
+    event.preventDefault();
+    const userData = {
+      username: username,
+      age: age,
+    };
+
+    console.log(userData);
+    setUsername("");
+    setAge("");
+  };
+  return (
+    <>
+      <Container>
+        <label className={styles.username} for="username">
+          Username
+        </label>
+        <input
+          className={styles["username-field"]}
+          onChange={logUsername}
+          type="text"
+          id="username"
+          value={username}
+        ></input>
+        <label className={styles.age} for="age">
+          Age (Years)
+        </label>
+        <input
+          className={styles["age-field"]}
+          type="number"
+          id="age"
+          onChange={logAge}
+          value={age}
+        ></input>
+
+        <input
+          className={styles.submit}
+          type="button"
+          value="Add user"
+          onClick={submitData}
+        ></input>
+      </Container>
+      <RenderUser></RenderUser>
+    </>
   );
 };
 
---
+++ src/components/RenderUser.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import Container from "./UI/Container";
+
+const RenderUser = (props) => {
+  return <Container></Container>;
+};
+
+export default RenderUser;
